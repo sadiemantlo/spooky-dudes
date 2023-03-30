d924995f4b25d1f358f4dc412984443dbce56bc4
unknown <sashardoval@gmail.com>
2023-03-30 14:43:23 -0700

    added id to userData

--- seed/userData.js
+++ seed/userData.js
@@ -1,13 +1,16 @@
 [
   {
+    "id":"1",
     "username": "harry",
     "password": "password123"
   },
   {
+    "id":"2",
     "username": "hermione",
     "password": "password123"
   },
   {
+    "id":"3",
     "username": "hagrid",
     "password": "password123"
   }
