4bec6db4489a849a0180409444ac3a4138409ea0
unknown <sashardoval@gmail.com>
2023-03-30 18:58:40 -0700

    created story handlebars

--- seed/userData.js
+++ seed/userData.js
@@ -1,16 +1,13 @@
 [
   {
-    "id":"1",
     "username": "harry",
     "password": "password123"
   },
   {
-    "id":"2",
     "username": "hermione",
     "password": "password123"
   },
   {
-    "id":"3",
     "username": "hagrid",
     "password": "password123"
   }
