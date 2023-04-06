6056f39a942f0d781b7e86de3d7332813e6d7025
unknown <sashardoval@gmail.com>
2023-04-05 21:44:10 -0700

    fixed empty button bug

--- public/js/story.js
+++ public/js/story.js
@@ -1,11 +1,31 @@
-// function myFunction() {
-//   let hidden = document.getElementById("myDIV");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
+function buttonEmpty() {
+  const hideText2= document.querySelector("div#scene-button2 button a");
+  const hideText3= document.querySelector("div#scene-button3 button a");
+  const hideButton2 = document.getElementById("scene-button2");
+  const  hideButton3= document.getElementById("scene-button3");
+  // if (hideButton === "@" ) {
+  //   console.log("The element has  content.");
+  // } else {
+  //   console.log("The element has content:" + hideButton.innerHTML);
+  // }
+  // if (hideButton.innerText.value === null) {
+  //   secondButton.style.display = "none";
+  // }
+   if (hideText2.innerHTML === "@") {
+    hideButton2.style.display = "none"
+  }
+  if (hideText3.innerHTML === "@") {
+    hideButton3.style.display = "none"
+  }
+//{{scene.choice2.[1]}}
+//{{scene.choice3.[1]}}
+
+  //test 
+ // hideButton2.style.display = "none";
+ // hideButton3.style.display = "none";
+}
+buttonEmpty()
+console.log();
 // document
 //   .querySelector('#start')
 //   .addEventListener('#start', handleSignupSubmit);
--- seed/sceneData.js
+++ seed/sceneData.js
@@ -6,8 +6,8 @@ const sceneData = [
     title: "Prendergast Manor",
     text: "Only the bravest enter the manor. The luckiest survive. Which one are you?",
     choice1:["1", "start"],
-    choice2:["",""],
-    choice3:["",""]
+    choice2:["","@"],
+    choice3:["","@"]
   },
   {
     id: 1,
@@ -15,8 +15,8 @@ const sceneData = [
     title: "Start",
     text: "You and your friends stand at the doorstep of Prendergast manor. Everyone in town knows about this place, or the rumors anyway. The previous owner, Lord Prendergast, was as eccentric as he was rich. When he died, his will stated that whoever could last a whole night in the manor would earn the rights to Lord Prendergast's lavish fortune. You and your friends agreed to stay the night together, then split your winnings. Unbeknownst to your group, the mansion is a death trap. You all look at each other, daring the other to back out before it is too late. But no one does.",
     choice1:["2","Enter the Mansion"],
-    choice2:["",""],
-    choice3:["",""]
+    choice2:["","@"],
+    choice3:["","@"]
   },
   {
     id:2,
@@ -34,7 +34,7 @@ const sceneData = [
     text: "Twinkies don't expire right? You make your way to the kitchen. The floorboards whine with each step you take, piercing the manor's heavy silence. Unfortunately the kitchen was emptied out long ago. You do notice, however, a waft of cold air coming from what looks to be a wine cellar. Weird. You wouldn't turn down an expensive cask of wine though.",
   choice1:["5","Go down to the Cellar"],
   choice2:["2","Keep Exploring the Manor"],
-  choice3:["",""]
+  choice3:["","@"]
   },
   {
     id: 4,
@@ -43,7 +43,7 @@ const sceneData = [
     text: "Libraries are safe. Maybe you and your friends can camp out there for the night. You go to scout it out. Despite the state of the rest of the manor, the library seems to be in good condition. Books line the shelves neatly, and ladders ascend to the top of the room’s vaulted ceilings. Out of the corner of your eye, you notice a brief rush of movement as something skitters up a ladder and disappears behind one of the shelves. You notice a draft from behind this bookshelf, maybe there's a secret door?",
     choice1:["8","Climb the Ladder"],
     choice2:["2","This is creepy. Go back to the Foyer"],
-    choice3:["",""]
+    choice3:["","@"]
   },
   {
     id:5,
@@ -51,8 +51,8 @@ const sceneData = [
     title: "The Cellar",
     text: "You open the cellar door to find a stairwell that leads undergrown in pitch darkness. You take a deep breath and decide to forge ahead. When you reach the bottom, you realize this isn't an ordinary cellar. Strange medieval devices clutter the room. Despite the lack of ventilation, you can't help but notice another rush of cold air. This place is creepy.",
     choice1:["9","Run up the stairs as fast as you can!"],
-    choice2:["",""],
-    choice3:["",""]
+    choice2:["","@"],
+    choice3:["","@"]
   },
   {
     id: 6,
@@ -61,7 +61,7 @@ const sceneData = [
     text: "Perhaps if you stay outside, the horrors of the mansion won't reach you. You step into the overgrown garden. In the distance you notice a strange golden glow. It's coming from the cemetery! Perhaps the Prendergast fortune is hidden there.",
     choice1:["7","Go to the Cemetery"],
     choice2:["2","Keep exploring the Mansion"],
-    choice3:["",""]
+    choice3:["","@"]
   },
   {
     id: 7,
@@ -69,8 +69,8 @@ const sceneData = [
     title: "The Cemetery",
     text: "You push open the creaky iron gate. The grass and trees are terribly overgrown, but the glowing golden light is unmistakable now. You rush forward. Maybe you can trick your way out of staying the night here. Grave robbing is a victim-less crime right?",
     choice1:["10","..."],
-    choice2:["",""],
-    choice3:["",""]
+    choice2:["","@"],
+    choice3:["","@"]
   },
 
   {
@@ -80,7 +80,7 @@ const sceneData = [
     text: "As you climb you notice a faint light behind the bookshelf peeking between the books. You push the books aside and find a peculiar hidden reading room. In the center of the table you see a ouija board. As you move closer you notice the planchette moving over the board on it's own. You put your hands on the planchette and it spells out. “<user's name>, my manor is yours now, for a price.” Do you agree?",
     choice1:["11","Yes!"],
     choice2:["2","No!"],
-    choice3:["",""]
+    choice3:["","@"]
   },
   {
     id: 9,
@@ -89,7 +89,7 @@ const sceneData = [
     text: "Forget the wine, you book it up the dark stairway, your sight set on the dim light of the kitchen above you. You almost reach the top when a rush of cold air hits you. Out of nowhere you feel icy hands claw at your shoulder blades, lifting you up by the back of your shirt. Your shoes scuff the stone staircase as you kick out at nothing. “Let go of me!” You shout, and the hands release you, sending you into a free fall down to the cellar far below you. Everything goes black.",
     choice1:["2","Try Again?"],
     choice2:["/select","Accept your fate"],
-    choice3:["",""]
+    choice3:["","@"]
   },
   {
     id: 10,
@@ -98,7 +98,7 @@ const sceneData = [
     text: "Just as the thought crosses your mind, and undead arm bursts from the ground and latches onto you leg. You cry out, and your ankle twists painfully as the ghoul's strong grip pulls you down. Darkness quickly overtakes you as your body is dragged into the earth. Button: Try again?",
     choice1:["2","Try Again?"],
     choice2:["/select","Accept your fate"],
-    choice3:["",""]
+    choice3:["","@"]
   },
   {
     id: 11,
@@ -106,8 +106,8 @@ const sceneData = [
     title: "You Survived!",
     text: "You agree. The whole manor seems to start shaking, books falling from the shelves. One particularly large tome hits your head and you black out. When you wake up, you notice daylight streaming into the library. A nearby chest filled to the brim with Lord Prendergast's fortune sits open. You go to find your friends, but they have all mysteriously disappeared. The following days are filled with fame and luxury, but you never see your friends again. Perhaps that was the price for your survival.",
     choice1:["/select","The End"],
-    choice2:["",""],
-    choice3:["",""]
+    choice2:["","@"],
+    choice3:["","@"]
   },
 ];
 
