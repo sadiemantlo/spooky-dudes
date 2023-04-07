98ae9d0dfcdb5ccd86388342ebb389a7362d9312
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-04-06 18:18:04 -0700

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const env       = process.env.NODE_ENV || 'development';
+const config    = require(__dirname + '/../config/config.json')[env];
 
 // create connection to the db. Connect to JWASDB instance if env var provided.
 // (Heroku creates this variable when you provision a JAWSDB instance.)
@@ -12,7 +14,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PW,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
       port: 3306,
     }
